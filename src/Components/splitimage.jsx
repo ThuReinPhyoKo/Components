85178ff069a7293d71004b802ce84410c730b881
ThuReinPhyoKo <118418687+ThuReinPhyoKo@users.noreply.github.com>
2025-08-20 17:45:00 +0630

    clip-path splitimage

---
+++ src/Components/splitimage.jsx
@@ -0,0 +1,30 @@
+export default function SplitImage(){
+    return (
+        <>
+            <div id="container" className="w-full h-[100vh] bg-amber-200"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split1.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-300"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split2.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-400"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split3.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-500"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split4.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-600"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split5.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-700"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split6.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-800"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split7.jpg" alt="Model" />
+            </div>
+            <div id="container" className="w-full h-[100vh] bg-amber-900"   >
+                <img className="fixed rounded-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover object-center w-[300px] h-[450px]" src="/splitimage/split8.jpg" alt="Model" />
+            </div>
+        </>
+    )
+}
